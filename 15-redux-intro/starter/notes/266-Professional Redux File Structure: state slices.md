69a25ff42ceff901c32b3ec992ed48037dadeb33
lucas30-c <96409387+lucas30-c@users.noreply.github.com>
2024-08-19 20:58:28 +1000

    266- Professional Redux File Structure: State Slices

---
+++ 15-redux-intro/starter/notes/266-Professional Redux File Structure: state slices.md
@@ -0,0 +1,4 @@
+## Slice
+A slice is a piece or just a part of the total state. The entire state lives in the store, then we can take one slice of that state.
+
+In each slice, we co-locate as much as the Redux logic as possible in one file, so we don't have to jump around between files.
--- 15-redux-intro/starter/src/index.js
+++ 15-redux-intro/starter/src/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-import "./store";
+import store from "./store";
+
+store.dispatch({ type: "account/deposit", payload: 8800 });
+store.dispatch({type: "customer/createCustomer", payload: { fullName: "Finn", nationalID: "1234567890", createdAt: new Date().toISOString() }})
+console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
