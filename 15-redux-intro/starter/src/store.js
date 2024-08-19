0ff70dc16d5d45403f8c8707685ed867ca88ab4f
lucas30-c <96409387+lucas30-c@users.noreply.github.com>
2024-08-19 16:57:35 +1000

    264- Working with Action Creators

--- 15-redux-intro/starter/src/store.js
+++ 15-redux-intro/starter/src/store.js
@@ -20,7 +20,7 @@ function reducer(state = initialState, action) {
         loanPurpose: action.payload.purpose,
         balance: state.balance + action.payload.amount,
       };
-    case "accout/payLoan":
+    case "account/payLoan":
       return {
         ...state,
         loan: 0,
@@ -38,17 +38,39 @@ const store = createStore(reducer);
 console.log("Hey Redux");
 
 // Problem here: In Redux, we actually do not write action type like this
-store.dispatch({ type: "account/deposit", payload: 100 });
-console.log(store.getState());
+// store.dispatch({ type: "account/deposit", payload: 100 });
+// console.log(store.getState());
 
-store.dispatch({ type: "account/withdraw", payload: 50 });
-console.log(store.getState());
+// store.dispatch({ type: "account/withdraw", payload: 50 });
+// console.log(store.getState());
 
-store.dispatch({
-  type: "account/requestLoan",
-  payload: { amount: 1000, purpose: "Home Renovation" },
-});
-console.log(store.getState());
+// store.dispatch({
+//   type: "account/requestLoan",
+//   payload: { amount: 1000, purpose: "Home Renovation" },
+// });
+// console.log(store.getState());
 
-store.dispatch({ type: "account/payLoan" });
-console.log(store.getState());
+// store.dispatch({ type: "account/payLoan" });
+// console.log(store.getState());
+
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+function requestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: { amount, purpose },
+  };
+}
+function payLoan() {
+  return { type: "account/payLoan" };
+}
+
+store.dispatch(deposit(100));
+store.dispatch(withdraw(50)); // { balance: 50, loan: 0, loanPurpose: "" }
+store.dispatch(requestLoan(1000, "Home Renovation"));
+store.dispatch(payLoan());
+console.log(store.getState()); 
