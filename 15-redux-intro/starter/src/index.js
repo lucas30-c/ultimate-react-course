6a8c0b23a868242bbe3713840a12b174625026d1
lucas30-c <96409387+lucas30-c@users.noreply.github.com>
2024-08-24 17:08:14 +1000

    282- Setting up project-vite-"Fast React Pizza Co."

--- 15-redux-intro/starter/src/index.js
+++ 15-redux-intro/starter/src/index.js
@@ -5,15 +5,6 @@ import { Provider } from "react-redux";
 
 import store from "./store";
 
-// store.dispatch({ type: "account/deposit", payload: 8800 });
-// store.dispatch({
-//   type: "customer/createCustomer",
-//   payload: {
-//     fullName: "Finn",
-//     nationalID: "1234567890",
-//     createdAt: new Date().toISOString(),
-//   },
-// });
 console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
