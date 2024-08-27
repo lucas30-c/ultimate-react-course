6aaffa87f83d96c1d9ede0218ec3d3e4cbbc3964
lucas30-c <96409387+lucas30-c@users.noreply.github.com>
2024-08-27 18:00:52 +1000

    286- build the app layout: createBrowserRouter + 286. Building the AppLayout(Nested Route)
    createBrowserRouter([
      {
        path: "/",
        element: <Home />,
      },
      {
        path: "/menu",
        element: <Menu />,
      },
      {
        path: "/cart",
        element: <Cart />,
      },
      {
        path: "/order/new",
        element: <CreateOrder />,
      },
      {
        path: "/order/:orderId",
        element: <Order />,
      },
    ]);
    
    export default function App() {
      return <RouterProvider router={router} />;
    }
    
    import { createBrowserRouter, RouterProvider } from "react-router-dom";
    
    import Home from "./features/ui/Home";
    import Menu from "./features/menu/Menu";
    import Cart from "./features/cart/Cart";
    import CreateOrder from "./features/order/CreateOrder";
    import Order from "./features/order/Order";
    import AppLayout from "./features/ui/AppLayout";
    
    // https://dev.to/tywenk/how-to-use-nested-routes-in-react-router-6-4jhd
    
    const router = createBrowserRouter([
      {
        element: <AppLayout />,
        children: [
          {
            path: "/",
            element: <Home />,
          },
          {
            path: "/menu",
            element: <Menu />,
          },
          {
            path: "/cart",
            element: <Cart />,
          },
          {
            path: "/order/new",
            element: <CreateOrder />,
          },
          {
            path: "/order/:orderId",
            element: <Order />,
          },
        ],
      },
    ]);
    
    export default function App() {
      return <RouterProvider router={router} />;
    }
    
    In the code above, the AppLayout has child layouts from Home to Order
    
    export default function AppLayout() {
      return (
        <div>
          <Header />
    
          <main>
            <Outlet />
          </main>
    
          <CartOverview />
        </div>
    
      );
    }

--- projects/fast-react-pizza/src/App.jsx
+++ projects/fast-react-pizza/src/App.jsx
@@ -1,3 +1,42 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./features/ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+import AppLayout from "./features/ui/AppLayout";
+
+// https://dev.to/tywenk/how-to-use-nested-routes-in-react-router-6-4jhd
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+      },
+    ],
+  },
+]);
+
 export default function App() {
-  return <div>Hello vite</div>;
+  return <RouterProvider router={router} />;
 }
